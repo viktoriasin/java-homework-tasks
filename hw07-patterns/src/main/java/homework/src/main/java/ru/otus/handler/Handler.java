79ac354944ee9f8ef91eacf42a9533baa4b950ce
Victoria <vicktoria.sin@yandex.ru>
2025-03-18 23:06:16 +0300

    init commit

---
+++ hw07-patterns/src/main/java/homework/src/main/java/ru/otus/handler/Handler.java
@@ -0,0 +1,12 @@
+package homework.src.main.java.ru.otus.handler;
+
+import ru.otus.listener.Listener;
+import ru.otus.model.Message;
+
+public interface Handler {
+    Message handle(Message msg);
+
+    void addListener(Listener listener);
+
+    void removeListener(Listener listener);
+}
