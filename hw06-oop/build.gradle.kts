235911a96f0b04698eb7b601333bc469024ff49a
Victoria <vicktoria.sin@yandex.ru>
2025-03-18 15:42:51 +0300

    delete unesessary dependencies

--- hw06-oop/build.gradle.kts
+++ hw06-oop/build.gradle.kts
@@ -1,6 +1,4 @@
 dependencies {
-    implementation ("ch.qos.logback:logback-classic")
-
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testImplementation ("org.junit.jupiter:junit-jupiter-engine")
     testImplementation ("org.assertj:assertj-core")
