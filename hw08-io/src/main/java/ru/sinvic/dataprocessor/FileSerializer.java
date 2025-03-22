a28c17547c3969404cc1c6822b1da390be0749fd
Victoria <vicktoria.sin@yandex.ru>
2025-03-22 20:08:11 +0300

    implement serializer

--- hw08-io/src/main/java/ru/sinvic/dataprocessor/FileSerializer.java
+++ hw08-io/src/main/java/ru/sinvic/dataprocessor/FileSerializer.java
@@ -1,13 +1,28 @@
 package ru.sinvic.dataprocessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
-    public FileSerializer(String fileName) {}
+    private final String fileName;
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
 
     @Override
     public void serialize(Map<String, Double> data) {
         // формирует результирующий json и сохраняет его в файл
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+        try {
+            objectMapper.writeValue(new File(fileName), data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- hw08-io/src/test/java/ru/sinvic/dataprocessor/ProcessorTest.java
+++ hw08-io/src/test/java/ru/sinvic/dataprocessor/ProcessorTest.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.assertj.core.api.AssertionsForClassTypes;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -19,7 +18,6 @@ class ProcessorTest {
     // сам тест менять нельзя
 
     @Test
-    @Disabled("Чтобы прошел билд") // Эту аннотацию надо убрать
     @DisplayName("Из файла читается json, обрабатывается, результат сериализуется в строку")
     void processingTest(@TempDir Path tempDir) throws IOException {
         System.out.println(tempDir);
