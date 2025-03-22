402a49f77035fb472465141c65b958142aead1ee
Victoria <vicktoria.sin@yandex.ru>
2025-03-22 19:07:08 +0300

    add negative test

--- hw07-patterns/src/test/java/ru/sinvic/homework/processor/ProcessorThrowExceptionInEvenSecondTest.java
+++ hw07-patterns/src/test/java/ru/sinvic/homework/processor/ProcessorThrowExceptionInEvenSecondTest.java
@@ -1,9 +1,11 @@
 package ru.sinvic.homework.processor;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -17,12 +19,26 @@ class ProcessorThrowExceptionInEvenSecondTest {
     @Mock
     private DateTimeProvider dateTimeProvider;
 
+    Message message;
+
+    @BeforeEach
+    void startUp() {
+        message = new Message.Builder(1L).field2("x").build();
+    }
+
     @Test
     void testProcessorThrowExceptionInEvenSecond() {
         when(dateTimeProvider.getDate()).thenReturn(LocalDateTime.of(2025, 1, 1, 22, 22, 22));
         ProcessorThrowExceptionInEvenSecond processorThrowExceptionInEvenSecond =
                 new ProcessorThrowExceptionInEvenSecond(dateTimeProvider);
-        Message message = new Message.Builder(1L).field2("x").build();
         assertThrows(RuntimeException.class, () -> processorThrowExceptionInEvenSecond.process(message));
+    }
+
+    @Test
+    void testProcessorDoesNotThrowExceptionInUnEvenSecond() {
+        when(dateTimeProvider.getDate()).thenReturn(LocalDateTime.of(2025, 1, 1, 22, 22, 23));
+        ProcessorThrowExceptionInEvenSecond processorThrowExceptionInEvenSecond =
+                new ProcessorThrowExceptionInEvenSecond(dateTimeProvider);
+        assertDoesNotThrow(() -> processorThrowExceptionInEvenSecond.process(message));
     }
 }
