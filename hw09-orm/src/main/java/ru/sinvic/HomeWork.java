f5ad40ca2d7fb63ca37fd0328b2b0f511d4f7b88
Victoria <vicktoria.sin@yandex.ru>
2025-03-27 01:28:04 +0300

    initial commit

---
+++ hw09-orm/src/main/java/ru/sinvic/HomeWork.java
@@ -0,0 +1,75 @@
+package ru.sinvic;
+
+import javax.sql.DataSource;
+import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.sinvic.core.repository.executor.DbExecutorImpl;
+import ru.sinvic.core.sessionmanager.TransactionRunnerJdbc;
+import ru.sinvic.crm.datasource.DriverManagerDataSource;
+import ru.sinvic.crm.model.Client;
+import ru.sinvic.crm.model.Manager;
+import ru.sinvic.crm.service.DbServiceClientImpl;
+import ru.sinvic.crm.service.DbServiceManagerImpl;
+import ru.sinvic.jdbc.mapper.DataTemplateJdbc;
+import ru.sinvic.jdbc.mapper.EntityClassMetaData;
+import ru.sinvic.jdbc.mapper.EntitySQLMetaData;
+
+@SuppressWarnings({"java:S125", "java:S1481"})
+public class HomeWork {
+    private static final String URL = "jdbc:postgresql://localhost:5430/demoDB";
+    private static final String USER = "usr";
+    private static final String PASSWORD = "pwd";
+
+    private static final Logger log = LoggerFactory.getLogger(HomeWork.class);
+
+    public static void main(String[] args) {
+        // Общая часть
+        var dataSource = new DriverManagerDataSource(URL, USER, PASSWORD);
+        flywayMigrations(dataSource);
+        var transactionRunner = new TransactionRunnerJdbc(dataSource);
+        var dbExecutor = new DbExecutorImpl();
+
+        // Работа с клиентом
+        EntityClassMetaData<Client> entityClassMetaDataClient; // = new EntityClassMetaDataImpl();
+        EntitySQLMetaData entitySQLMetaDataClient = null; // = new EntitySQLMetaDataImpl(entityClassMetaDataClient);
+        var dataTemplateClient = new DataTemplateJdbc<Client>(
+            dbExecutor, entitySQLMetaDataClient); // реализация DataTemplate, универсальная
+
+        // Код дальше должен остаться
+        var dbServiceClient = new DbServiceClientImpl(transactionRunner, dataTemplateClient);
+        dbServiceClient.saveClient(new Client("dbServiceFirst"));
+
+        var clientSecond = dbServiceClient.saveClient(new Client("dbServiceSecond"));
+        var clientSecondSelected = dbServiceClient
+            .getClient(clientSecond.getId())
+            .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecond.getId()));
+        log.info("clientSecondSelected:{}", clientSecondSelected);
+
+        // Сделайте тоже самое с классом Manager (для него надо сделать свою таблицу)
+
+        EntityClassMetaData<Manager> entityClassMetaDataManager; // = new EntityClassMetaDataImpl();
+        EntitySQLMetaData entitySQLMetaDataManager = null; // = new EntitySQLMetaDataImpl(entityClassMetaDataManager);
+        var dataTemplateManager = new DataTemplateJdbc<Manager>(dbExecutor, entitySQLMetaDataManager);
+
+        var dbServiceManager = new DbServiceManagerImpl(transactionRunner, dataTemplateManager);
+        dbServiceManager.saveManager(new Manager("ManagerFirst"));
+
+        var managerSecond = dbServiceManager.saveManager(new Manager("ManagerSecond"));
+        var managerSecondSelected = dbServiceManager
+            .getManager(managerSecond.getNo())
+            .orElseThrow(() -> new RuntimeException("Manager not found, id:" + managerSecond.getNo()));
+        log.info("managerSecondSelected:{}", managerSecondSelected);
+    }
+
+    private static void flywayMigrations(DataSource dataSource) {
+        log.info("db migration started...");
+        var flyway = Flyway.configure()
+            .dataSource(dataSource)
+            .locations("classpath:/db/migration")
+            .load();
+        flyway.migrate();
+        log.info("db migration finished.");
+        log.info("***");
+    }
+}
---
+++ hw09-orm/src/main/java/ru/sinvic/jdbc/mapper/EntitySQLMetaData.java
@@ -0,0 +1,12 @@
+package ru.sinvic.jdbc.mapper;
+
+/** Создает SQL - запросы */
+public interface EntitySQLMetaData {
+    String getSelectAllSql();
+
+    String getSelectByIdSql();
+
+    String getInsertSql();
+
+    String getUpdateSql();
+}
