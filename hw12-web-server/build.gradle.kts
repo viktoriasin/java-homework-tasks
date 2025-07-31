65c523f49b67f569b7a44af628d51cd88c9f7078
Victoria <vicktoria.sin@yandex.ru>
2025-07-31 20:02:32 +0300

    add dependencies

--- hw12-web-server/build.gradle.kts
+++ hw12-web-server/build.gradle.kts
@@ -16,6 +16,17 @@ dependencies {
     testImplementation("org.assertj:assertj-core")
     testImplementation("org.mockito:mockito-junit-jupiter")
 
+    implementation("com.google.code.gson:gson")
+
+    implementation("org.eclipse.jetty.ee10:jetty-ee10-servlet")
+    implementation("org.eclipse.jetty:jetty-server")
+    implementation("org.eclipse.jetty.ee10:jetty-ee10-webapp")
+    implementation("org.eclipse.jetty:jetty-security")
+    implementation("org.eclipse.jetty:jetty-http")
+    implementation("org.eclipse.jetty:jetty-io")
+    implementation("org.eclipse.jetty:jetty-util")
+    implementation("org.freemarker:freemarker")
+
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
 }
