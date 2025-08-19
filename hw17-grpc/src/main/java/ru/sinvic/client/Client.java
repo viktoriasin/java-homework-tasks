23e9777ae4ff134471201e8413d1372effff1710
Victoria <vicktoria.sin@yandex.ru>
2025-08-19 17:43:09 +0300

    complete task

--- hw17-grpc/src/main/java/ru/sinvic/client/Client.java
+++ hw17-grpc/src/main/java/ru/sinvic/client/Client.java
@@ -1,7 +1,6 @@
 package ru.sinvic.client;
 
 import io.grpc.ManagedChannelBuilder;
-import java.util.concurrent.CountDownLatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.sinvic.ClientMessage;
@@ -11,6 +10,7 @@ public class Client {
     private static final Logger log = LoggerFactory.getLogger(Client.class);
     private static final String SERVER_HOST = "localhost";
     private static final int SERVER_PORT = 8190;
+    private static long currentValue;
 
     public static void main(String[] args) throws InterruptedException {
         var channel = ManagedChannelBuilder.forAddress(SERVER_HOST, SERVER_PORT)
@@ -19,14 +19,37 @@ public class Client {
 
         log.info("Client is starting...");
 
-        var latch = new CountDownLatch(1);
-        var newStub = RemoteServiceGrpc.newStub(channel);
-        newStub.getNumbers(
-                ClientMessage.newBuilder().setFirstValue(1L).setLastValue(30L).build(),
-                new ClientStreamObserver(latch));
+        RemoteServiceGrpc.RemoteServiceStub asyncStub = RemoteServiceGrpc.newStub(channel);
+        new Client().clientAction(asyncStub);
 
-        latch.await();
-
+        log.info("Client is shutting down...");
         channel.shutdown();
+    }
+
+    private void clientAction(RemoteServiceGrpc.RemoteServiceStub asyncStub) {
+        ClientStreamObserver clientStreamObserver = new ClientStreamObserver();
+        asyncStub.getNumbers(makeClientRequest(), clientStreamObserver);
+
+        for (int i = 0; i < 50; i++) {
+            log.info("Current value: {}", getNextValue(clientStreamObserver));
+            sleep();
+        }
+    }
+
+    private ClientMessage makeClientRequest() {
+        return ClientMessage.newBuilder().setFirstValue(1L).setLastValue(30L).build();
+    }
+
+    public long getNextValue(ClientStreamObserver clientStreamObserver) {
+        currentValue = currentValue + clientStreamObserver.getLastValueAndReset() + 1;
+        return currentValue;
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
